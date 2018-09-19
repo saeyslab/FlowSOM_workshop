e15ea4d496c80d08536ec57a521e6a0e9a678e39
Helena Todorov <helena_t@hotmail.fr>
2018-09-19 10:43:28 +0200

    Functions updates

--- R/installation.R
+++ R/installation.R
@@ -1,17 +1,19 @@
-install.packages("devtools")
-install.packages("Rtsne")
-install.packages("tidyverse")
+installation_flowSOMworkshop <- function(){
+  install.packages("devtools")
+  install.packages("Rtsne")
+  install.packages("tidyverse")
 
-# Bioconductor packages , to be installed through biocLite:
+  # Bioconductor packages , to be installed through biocLite:
 
-source("https://bioconductor.org/biocLite.R")
-biocLite("flowCore")
-biocLite("flowWorkspace")
-biocLite("flowAI")
-biocLite("flowDensity")
+  source("https://bioconductor.org/biocLite.R")
+  biocLite("flowCore")
+  biocLite("flowWorkspace")
+  biocLite("flowAI")
+  biocLite("flowDensity")
 
-# FlowSOM and flowSOM_workshop packages, to be installed directly from the
-# github repository:
+  # FlowSOM and flowSOM_workshop packages, to be installed directly from the
+  # github repository:
 
-devtools::install_github("saeyslab/FlowSOM")
-devtools::install_github("saeyslab/FlowSOM_workshop", build_vignettes = TRUE)
+  devtools::install_github("saeyslab/FlowSOM")
+  devtools::install_github("saeyslab/FlowSOM_workshop", build_vignettes = TRUE)
+}
