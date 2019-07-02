39acd77e60a25686377c4eb6d3cf23565a4ea291
svgassen <sofie.vangassen@ugent.be>
2019-07-02 17:07:29 +0200

    Bugfix in plot_MFIs

--- R/plot_MFIs.R
+++ R/plot_MFIs.R
@@ -1,16 +1,16 @@
 #'@export
 plot_cluster_MFIs <- function(fsom, names = NULL){
   MFIs <- GetMFIs(fsom)[, fsom$FlowSOM$map$colsUsed]
-  plot_MFIs(MFIs, names)
+  plot_MFIs(MFIs, fsom, names)
 }
 
 #'@export
 plot_metacluster_MFIs <- function(fsom, names = NULL){
   MFIs <- MetaclusterMFIs(fsom)[, fsom$FlowSOM$map$colsUsed]
-  plot_MFIs(MFIs, names)
+  plot_MFIs(MFIs, fsom, names)
 }
 
-plot_MFIs <- function(MFIs, names){
+plot_MFIs <- function(MFIs, fsom, names){
   colnames(MFIs) <- fsom$FlowSOM$prettyColnames[fsom$FlowSOM$map$colsUsed]
   if(!is.null(names)) rownames(MFIs) <- paste0(rownames(MFIs), " (", names, ")")
   pheatmap::pheatmap(MFIs)
