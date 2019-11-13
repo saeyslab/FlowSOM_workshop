ecb6d4b667e6ec07f86c3464819a36baf2896cfe
svgassen <sofie.vangassen@ugent.be>
2019-11-13 10:19:37 +0100

    Update to also be able to use an aggregated flowframe for the plot_aggregate function

--- R/plot_aggregate.R
+++ R/plot_aggregate.R
@@ -9,23 +9,36 @@
 #' @return nothing but a plot is generated
 #'
 #' @export
-plot_aggregate <- function(flowset,
+plot_aggregate <- function(input,
                            labels = NULL,
+                           channels = NULL,
+                           sample_names = NULL,
                            output_image = "aggregate.png",
-                           max_nm = 6*25000){
-  data <- flowCore::fsApply(flowset, function(ff){ ff@exprs })
-  cell_counts <- flowCore::fsApply(flowset, function(ff){ nrow(ff) })
-  file_values <- unlist(sapply(seq_len(length(cell_counts)),
-                                  function(i){
-                                    rep(i, cell_counts[i])
-                                  }))
+                           max_nm = 150000){
+  if(is(input, "flowSet")){
+    data <- flowCore::fsApply(input, function(ff){ ff@exprs })
+    cell_counts <- flowCore::fsApply(input, function(ff){ nrow(ff) })
+    file_values <- unlist(sapply(seq_len(length(cell_counts)),
+                                 function(i){
+                                   rep(i, cell_counts[i])
+                                 }))
+    ff <- input[[1]]
+  } else if(is(input, "flowFrame")){
+    data <- input@exprs
+    cell_counts <- table(data[,"File"])
+    file_values <- data[,"File"]
+    ff <- input
+  }
+
+  if(is.null(sample_names)) sample_names <- names(cell_counts)
+
   subset <- sample(seq_len(nrow(data)), min(max_nm, nrow(data)))
   data <- data[subset, ]
   file_values <- file_values[subset]
   file_values_scattered <- file_values + stats::rnorm(length(file_values),
                                                       0, 0.1)
 
-  channels <- colnames(data)
+  if(is.null(channels)) channels <- colnames(data)
 
   # Try to make the plot as square as possible
   nrows_in_plot <- floor(sqrt(length(channels)))
@@ -51,21 +64,21 @@ plot_aggregate <- function(flowset,
   }
   # Make a plot for every channel
   for(channel in channels){
-    print(FlowSOM::get_markers(flowset[[1]], channel))
+    print(FlowSOM::get_markers(ff, channel))
     # Empty plot, with ranges set correctly
     plot(0, type="n", xaxt="n",
          xlab = "",
-         ylab = FlowSOM::get_markers(flowset[[1]], channel),
+         ylab = FlowSOM::get_markers(ff, channel),
          cex.lab = 5,
          ylim = c(min(data[, channel]),
                   max(data[, channel])),
-         xlim = c(0, length(flowset)+1))
+         xlim = c(0, length(cell_counts)+1))
     # Add light grey vertical lines
-    abline(v = seq_len(length(flowset)), col="lightgrey")
-    # Add single stain names on x-axis
+    abline(v = seq_len(length(cell_counts)), col="lightgrey")
+    # Add file names on x-axis
     axis(side =  1,
-         at = seq_len(length(flowset)),
-         labels = rownames(cell_counts),
+         at = seq_len(length(cell_counts)),
+         labels = sample_names,
          las = 2,
          cex.axis = 3)
     # Add the actual data points
